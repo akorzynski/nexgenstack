9543bb1f64e50124db4d642309be27179fa954ab
Aleksander Korzynski <59058391+akorzynski@users.noreply.github.com>
2024-08-29 19:25:25 +0200

    Define RabbitMQ settings

--- nexgenstack/settings.py
+++ nexgenstack/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+EXCHANGE_NAME = 'x.compute_task_distributor'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
